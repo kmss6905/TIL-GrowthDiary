358abe583780e31d0ae9797060a410a803b7d00e
kmss6905 <kmss69052@naver.com>
2023-06-04 23:31:00 +0900

    two sum 완전탐색

--- algorithm/basic/twosum.py
+++ algorithm/basic/twosum.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
-    print('Hello algorithm')
+# twoSum 완전탐색
+def twoSum(nums, target):
+    n = len(nums)
+    for i in range(n-1):
+        for j in range(i+1, n-1):
+            if nums[i] + nums[j] == target:
+                return True
+    return False
+
+
+print(twoSum(nums=[4, 1, 9, 7, 5, 3, 16], target=14))

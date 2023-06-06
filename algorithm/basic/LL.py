602ca5f20ad831d3129073404324992fd692cae0
kmss6905 <kmss69052@naver.com>
2023-06-06 21:00:02 +0900

    Linked List remove 수정

--- algorithm/basic/LL.py
+++ algorithm/basic/LL.py
@@ -34,9 +34,35 @@ class LinkedList:
             current = current.next
         before.next = current.next
 
-    def insert(self, idx, value):
-        pass
+    # O(1)
+    def replace(self, idx, value):
+        new_node = Node(value)
+        current = self.head
+        before = None
+        if idx == 1:
+            before = self.head
+        for _ in range(idx):
+            if _ == idx-2:
+                before = current
+            current = current.next
+        new_node.next = current.next
+        before.next = new_node
 
+    # O(1) insert
+    def insert(self, idx, value):
+        new_node = Node(value)
+        current = self.head
+        before = None
+        if idx == 1:
+            before = self.head
+        for _ in range(idx):
+            current = current.next
+            if idx != 1:
+                before = current
+        new_node.next = current
+        before.next = new_node
+
+    # O(N)
     def print_all_element(self):
         enum = []
         current = self.head
@@ -50,7 +76,18 @@ ll = LinkedList()
 ll.append(1)
 ll.append(2)
 ll.append(3)
+ll.append(4)
+ll.append(5)
+ll.print_all_element()
+
+ll.insert(1, 10)
+
+ll.print_all_element()
+
+ll.replace(1, 7)
+
 ll.print_all_element()
 
 ll.remove(1)
+
 ll.print_all_element()

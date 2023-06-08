00de2504dc121c45d7b87910ab5754e4366055c9
kmss6905 <kmss69052@naver.com>
2023-06-08 18:25:13 +0900

    괄호쌍 문제(주석 추가)

--- algorithm/basic/스택/괄호쌍.py
+++ algorithm/basic/스택/괄호쌍.py
@@ -33,14 +33,18 @@ class Solution_Two(object):
     def isValid(self, s):
         stack = []
 
-        # 괄호 쌍을 딕셔너리로 표현 ( 아이디어 )
+        # 괄호 쌍을 딕셔너리로 표현 ( idea 1 )
         bracket_map = {
             ')': '(',
             '}': '{',
             ']': '['
         }
 
+        # O(N)
         for char in s:
+            # 문자열 포함 여부 확인 ( idea 2 ), 검사하려는 문자 char 가 '({['에 존재하는 지 확인하기 위해 문자열의
+            # 각 문자를 순회하며 동등성을 비교함 따라서 시간복잡도 O(N)
+            # 하지만 사실상 여기서는 O(1) 로 보고 풀어도 무관
             if char in '({[':
                 stack.append(char)
             elif char in ')}]':

1ba7b420246b763c138849773ea81ed7ecfe74d6
kmss6905 <kmss69052@naver.com>
2023-06-10 00:01:30 +0900

    baekjoon 10828 스택, 참고코드 추가

--- algorithm/basic/스택/백준/10828.py
+++ algorithm/basic/스택/백준/10828.py
@@ -5,7 +5,6 @@ answer = []
 for i in range(num):
     # input() 을 사용하면 시간초과 난다.(idea)
     a = sys.stdin.readline().split()
-
     commend = a[0]
     if commend == "push":
         stack.append(a[1])
@@ -26,3 +25,26 @@ for i in range(num):
             print(-1)
         else:
             print(stack[-1])
+
+        import sys
+
+        input = sys.stdin.readline
+
+# 다른 사람 풀이
+def sol():
+    stack = []
+    N = int(input())
+
+    for n in range(N):
+        command = input().rstrip()  # idea 2 ( 공백제거 )
+        if command == "pop":
+            print(stack.pop() if stack else -1)  # pythonic
+        elif command == "size":
+            print(len(stack))
+        elif command == "empty":
+            print(+(not stack))
+        elif command == "top":
+            print(stack[-1] if stack else -1)
+        else:
+            _, x = command.split()  # idea 3
+            stack.append(x)

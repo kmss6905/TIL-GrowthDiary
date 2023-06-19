2cf937cc48f8de8ae1a45a55b670e4bb39843159
kmss6905 <kmss69052@naver.com>
2023-06-19 22:25:35 +0900

    beakjoon 2309

--- algorithm/baekjoon/2309.py
+++ algorithm/baekjoon/2309.py
@@ -1,1 +1,16 @@
 # 일곱난쟁이
+# 1. 브루스포스
+array = []
+for i in range(9):
+    array.append(int(input()))
+array.sort()
+sum_ = sum(array)
+for i in range(len(array)):
+    for j in range(i + 1, len(array)):
+        if sum_ - array[i] - array[j] == 100:
+            for k in range(len(array)):
+                if k == i or k == j:
+                    pass
+                else:
+                    print(array[k])
+            exit()

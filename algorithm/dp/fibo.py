3c2947a61f685d4f0b09ea108a01392e2289b23d
kmss6905 <kmss69052@naver.com>
2023-07-05 19:24:07 +0900

    dp top-down fibo

---
+++ algorithm/dp/fibo.py
@@ -0,0 +1,14 @@
+# DP, Top-down
+import time
+
+memo = {}
+def fibo(n):
+  if n == 1 or n == 2:
+       return 1
+  if n not in memo:
+       memo[n] = fibo(n-1) + fibo(n-2)
+  return memo[n]
+
+
+if __name__ == '__main__':
+  fibo(500)

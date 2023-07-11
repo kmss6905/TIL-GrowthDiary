63f5d12a18eb6539b4e5336040633bc3c1868612
kmss6905 <kmss69052@naver.com>
2023-07-11 13:55:24 +0900

    edit boj 2468

--- algorithm/baekjoon/2468.py
+++ algorithm/baekjoon/2468.py
@@ -1,43 +1,48 @@
-
-import sys
 from collections import deque
+import sys
 
-dx = [-1, 0, 0, 1]
-dy = [0, -1, 1, 0]
+def sol():
 
-
-def bfs(i, j, k):
-    queue = deque()
-    queue.append((i, j))
-    visited[i][j] = True
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if region[nx][ny] > k and visited[nx][ny] == False:
-                    queue.append((nx, ny))
-                    visited[nx][ny] = True
-
-
-N = int(sys.stdin.readline())
-region = []
-region_value = {0}
-for _ in range(N):
-    small_region = list(map(int, sys.stdin.readline().split()))
-    region_value.update(small_region)
-    region.append(small_region)
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    def bfs(y, x, rain):
+        queue = deque()
+        queue.append((y, x))
+        visited[y][x] = True
+        while queue:
+            cy, cx = queue.popleft()
+            for dy, dx in directions:
+                next_x = cx + dx
+                next_y = cy + dy
+                if (
+                    0 <= next_x < n
+                    and 0 <= next_y < n
+                    and region[next_y][next_x] > rain
+                    and (visited[next_y][next_x] == False)
+                ):
+                    queue.append((next_y, next_x))
+                    visited[next_y][next_x] = True
 
 
-answer = 0
-for k in region_value:
-    count = 0
-    visited = [[False] * N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            if region[i][j] > k and visited[i][j] == False:
-                count += 1
-                bfs(i, j, k)
-    answer = max(answer, count)
-print(answer)
+    n = int(sys.stdin.readline())
+    region = []
+    region_value = {0}
+    for _ in range(n):
+        row = list(map(int, sys.stdin.readline().split()))
+        region_value.update(row)
+        region.append(row)
+
+    answer = 0
+    for k in region_value:
+        count = 0
+        visited = [[False] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if visited[i][j] == False and region[i][j] > k:
+                    count += 1
+                    bfs(i, j, k)
+        answer = max(answer, count)
+    return answer
+
+
+if __name__ == '__main__':
+    print(sol())

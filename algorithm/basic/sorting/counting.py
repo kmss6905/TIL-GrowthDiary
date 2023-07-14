1489076137405707c7ed624e4a468df32fb868ec
kmss6905 <kmss69052@naver.com>
2023-07-14 15:29:14 +0900

    add counting sort comment

--- algorithm/basic/sorting/counting.py
+++ algorithm/basic/sorting/counting.py
@@ -1,12 +1,14 @@
 from itertools import count
 
-
+# 모든 원소의 값이 0보다 크거나 같다고 가정
 array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
-count_array = [0]*10
+# 모든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
+count_arr = [0]*(max(array)+1)
+
+
 for i in array:
-    count_array[i] += 1
+    count_arr[array[i]] += 1 # 각 데이터에 해당하는 인덱스의 값 증가
 
-for i,v in enumerate(count_array):
-    print(str(i)*v, end="")
-
-    
+for i in range(len(count_arr)):
+    for j in range(count_arr[i]): # 리스트에 기록된 정렬 정보 확인
+        print(i, end=' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력

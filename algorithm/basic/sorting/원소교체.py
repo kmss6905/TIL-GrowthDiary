dd0bd8068a8b43e101691e64ce43aa91b7594c74
kmss6905 <kmss69052@naver.com>
2023-07-14 16:00:01 +0900

    edit  원소교체

--- algorithm/basic/sorting/원소교체.py
+++ algorithm/basic/sorting/원소교체.py
@@ -3,4 +3,11 @@ a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 a.sort() # nlogn
 b.sort(reverse=True) # nlogn
-print(sum(b[0:k]) + sum(a[k:]))
+
+for i in range(k):
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+    else: 
+        break
+
+print(sum(a))

cdbe1f1bfdcc1152f282cbba0e6eed1bfa004e0c
kmss6905 <kmss69052@naver.com>
2023-08-26 00:15:50 +0900

    [spring] interceptor logging

--- spring-security/login-session/src/main/java/com/example/login/config/WebConfig.java
+++ spring-security/login-session/src/main/java/com/example/login/config/WebConfig.java
@@ -2,15 +2,18 @@ package com.example.login.config;
 
 import com.example.login.filter.LogFilter;
 import com.example.login.filter.LoginCheckerFilter;
+import com.example.login.interceptor.LogInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-  @Bean
+//  @Bean
   public FilterRegistrationBean logFilter() {
     FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
     filterRegistrationBean.setFilter(new LogFilter());
@@ -26,5 +29,13 @@ public class WebConfig {
     filterRegistrationBean.setOrder(2);
     filterRegistrationBean.addUrlPatterns("/*");
     return filterRegistrationBean;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/css/**", "/*.ico", "/error");
   }
 }

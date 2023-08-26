440ca958f2cf63b360c3a202574f9e0ddc2f7b95
kmss6905 <kmss69052@naver.com>
2023-08-26 14:37:40 +0900

    [spring] interceptor , remove useless import

--- spring-security/login-session/src/test/java/com/example/login/interceptor/LoginCheckerInterceptorTest.java
+++ spring-security/login-session/src/test/java/com/example/login/interceptor/LoginCheckerInterceptorTest.java
@@ -1,16 +1,10 @@
 package com.example.login.interceptor;
 
-import com.example.login.controller.httpsession.SessionConst;
-import com.example.login.controller.session.SessionUser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;

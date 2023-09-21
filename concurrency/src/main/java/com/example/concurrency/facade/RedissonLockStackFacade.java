b65a232de4619f6b5ccb4facb0ca987a6fd83661
kmss6905 <kmss69052@naver.com>
2023-09-21 12:24:11 +0900

    add RedissonLock

---
+++ concurrency/src/main/java/com/example/concurrency/facade/RedissonLockStackFacade.java
@@ -0,0 +1,37 @@
+package com.example.concurrency.facade;
+
+import com.example.concurrency.application.stock.StockService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedissonLockStackFacade {
+
+  private final RedissonClient redissonClient;
+  private final StockService stockService;
+
+
+  public RedissonLockStackFacade(RedissonClient redissonClient, StockService stockService) {
+    this.redissonClient = redissonClient;
+    this.stockService = stockService;
+  }
+
+  public void decrease(Long id, Long quantity) {
+    RLock lock = redissonClient.getLock(id.toString());
+    try {
+      boolean available = lock.tryLock(15, 1, TimeUnit.SECONDS);
+      if (!available) {
+        System.out.println("Lock 흭득 실패");
+        return;
+      }
+      stockService.decrease(id, quantity);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    } finally {
+      lock.unlock();
+    }
+  }
+}

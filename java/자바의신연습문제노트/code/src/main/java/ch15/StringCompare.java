5a4258783ae22b97d732d035bd28774d9debafe0
kmss6905 <kmss69052@naver.com>
2024-04-17 16:11:24 +0900

    자바의신 15장 - String 코드 추가

--- java/자바의신연습문제노트/code/src/main/java/ch15/StringCompare.java
+++ java/자바의신연습문제노트/code/src/main/java/ch15/StringCompare.java
@@ -14,6 +14,14 @@ public class StringCompare {
         "서울시 구로구 개봉동"
     };
     sample.contentEuqalsTest(address);
+    sample.checkIndexOf();
+    sample.checkLastIndexOf();
+    sample.checkCharAt();
+    sample.checkSubString();
+    sample.checkSplit();
+    sample.checkTrim();
+    sample.stringAppend();
+    sample.stringBuffer();
   }
 
   public void checkString() {
@@ -49,6 +57,7 @@ public class StringCompare {
     if (text.equals(text2)) {
       System.out.println("text.equals(text2) result is same");
     }
+    System.out.println("---------------");
   }
 
   public void contentEuqalsTest(String[] address) {
@@ -66,7 +75,77 @@ public class StringCompare {
     }
     System.out.println("startCount = " + startCount);
     System.out.println("endCount = " + endCount);
+    System.out.println("---------------");
   }
 
+  public void checkIndexOf() {
+    String text = "Java technology is both a programing language and a platform";
+    System.out.println(text.indexOf('a'));
+    System.out.println(text.indexOf("a "));
+    System.out.println(text.indexOf('a', 20));
+    System.out.println(text.indexOf("a ", 20));
+    System.out.println(text.indexOf('z'));
+    System.out.println("---------------");
+  }
 
+  public void checkLastIndexOf() {
+    String text = "Java technology is both a programing language and a platform";
+    System.out.println(text.lastIndexOf('a'));
+    System.out.println(text.lastIndexOf("a "));
+    System.out.println(text.lastIndexOf('a', 20));
+    System.out.println(text.lastIndexOf("a ", 20));
+    System.out.println(text.lastIndexOf('z'));
+    System.out.println("---------------");
+  }
+
+  public void checkCharAt() {
+    String text = "Java technology is both a programing language and a platform";
+    System.out.println(text.charAt(0));
+    System.out.println("---------------");
+  }
+
+  public void checkSubString() {
+    String text = "Java technology";
+    System.out.println("'Java technology'.substring(5) = " + text.substring(5) );
+    String tech = text.substring(5, 9);
+    System.out.println("'Java technology'.substring(5, 9); = " + tech);
+    System.out.println("---------------");
+  }
+
+  public void checkSplit() {
+    String text = "Java technology is both a programing language and a platform";
+    String[] splitArray = text.split(" ");
+    for (String s : splitArray) {
+      System.out.println(s);
+    }
+    System.out.println("---------------");
+  }
+
+  public void checkTrim() {
+    String[] strings = {
+        " a", "b ", "        c", "d        ", "e     f", "    "
+    };
+
+    for (String string : strings) {
+      System.out.println("[" + string + "]" );
+      System.out.println("[" + string.trim() + "]" );
+    }
+    System.out.println("---------------");
+  }
+
+  public void stringAppend() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Hello");
+    sb.append(" world");
+    System.out.println(sb);
+    System.out.println("---------------");
+  }
+
+  public void stringBuffer() {
+    StringBuffer stringBuffer = new StringBuffer();
+    stringBuffer.append("Hello ");
+    stringBuffer.append(" world");
+    System.out.println(stringBuffer);
+    System.out.println("---------------");
+  }
 }

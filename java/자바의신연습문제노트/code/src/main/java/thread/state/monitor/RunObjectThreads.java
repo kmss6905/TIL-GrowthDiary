0e7108941951d52c173b8516184d415ad334c156
kmss6905 <kmss69052@naver.com>
2024-04-25 22:16:13 +0900

    여러 스레드가 monitor 객체 공유해서 사용하는 예제

--- java/자바의신연습문제노트/code/src/main/java/thread/state/monitor/RunObjectThreads.java
+++ java/자바의신연습문제노트/code/src/main/java/thread/state/monitor/RunObjectThreads.java
@@ -4,8 +4,41 @@ public class RunObjectThreads {
 
   public static void main(String[] args) {
     RunObjectThreads sample = new RunObjectThreads();
-    sample.checkThreadState2();
+//    sample.checkThreadState2();
+    sample.checkThreadState3();
   }
+
+  public void checkThreadState3() {
+    Object monitor = new Object();
+    StateThread thread = new StateThread(monitor);
+    StateThread thread2 = new StateThread(monitor);
+
+    try {
+      System.out.println("thread state=" + thread.getState());
+      thread.start();
+      thread2.start();
+      System.out.println("thread state(after start)=" + thread.getState());
+      Thread.sleep(100);
+      System.out.println("thread state(after 0.1 sec)=" + thread.getState());
+
+      synchronized (monitor) {
+        monitor.notify();
+      }
+
+      Thread.sleep(100);
+
+      System.out.println("thread state(after notify)="+ thread.getState());
+
+      thread.join();
+
+      System.out.println("thread state(after join)=" + thread.getState());
+      System.out.println("thread2 state(after join)=" + thread2.getState());
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+
 
   public void checkThreadState2() {
     // 모니터를 밖에서 관리한다.

a5d5a674074ca6b572906a602c8910ac1e2b72ff
kmss6905 <kmss69052@naver.com>
2024-05-18 13:42:27 +0900

    람다식 개념

--- java/자바의신연습문제노트/code/src/main/java/thread/singleton/SingletonObject2.java
+++ java/자바의신연습문제노트/code/src/main/java/thread/singleton/SingletonObject2.java
@@ -11,7 +11,9 @@ public class SingletonObject2 {
   public synchronized static SingletonObject2 getInstance() throws InterruptedException {
     if (singleObject2 == null) {
       synchronized (SingletonObject2.class) {
-        singleObject2 = new SingletonObject2();
+        if (singleObject2 == null) {
+          singleObject2 = new SingletonObject2();
+        }
       }
     }
     return singleObject2;

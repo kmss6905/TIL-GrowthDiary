8b8354145a96fc47670cc0007e8854513b327991
김민식 <kmss69052@naver.com>
2024-06-12 23:06:30 +0900

    [인프런강의] PessimisticLock service 주석 추가

--- distributelock/src/main/java/database/distributelock/service/PessimisticLockStockService.java
+++ distributelock/src/main/java/database/distributelock/service/PessimisticLockStockService.java
@@ -16,8 +16,9 @@ public class PessimisticLockStockService {
 
   @Transactional
   public void decrease(Long id, Long quantity) {
+    // 실제 db 에 락을 걸기 때문에 성능저하가 있을 수 있다.
+    // for update 쿼리에 의해 설정된 모든 잠금은 트랜잭션이 커밋되거나 롤백될 때 해제된다.
     Stock stock = stockRepository.findByWithPessimisticLock(id);
     stock.decrease(quantity);
-    stockRepository.save(stock);
   }
 }

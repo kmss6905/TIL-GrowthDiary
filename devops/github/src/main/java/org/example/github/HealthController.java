7342784cb757a48e86812df4d5e34d19d45fb83b
김민식 <kmss69052@naver.com>
2024-07-23 22:20:48 +0900

    add deploy job in workflow yml

--- devops/github/src/main/java/org/example/github/HealthController.java
+++ devops/github/src/main/java/org/example/github/HealthController.java
@@ -1,13 +1,20 @@
 package org.example.github;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
+  @Value("${spring.application.name}")
+  String appName;
+
+  @Value("${app.version}")
+  String appVersion;
+
   @GetMapping("/health")
   public String ok() {
-    return "ok";
+    return appName + ": " + appVersion;
   }
 }

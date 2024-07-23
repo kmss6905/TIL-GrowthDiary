b56b73db7711e1304700bdcc15440cfd512b4ac4
김민식 <kmss69052@naver.com>
2024-07-23 21:22:06 +0900

    컨트롤러 어노테이션 추가

--- devops/github/src/main/java/org/example/github/HealthController.java
+++ devops/github/src/main/java/org/example/github/HealthController.java
@@ -1,7 +1,9 @@
 package org.example.github;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthController {
 
   @GetMapping("/health")
